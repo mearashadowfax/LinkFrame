389b6ec61283a1f04baa109c67472f7a181f83e4
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-10-10 23:54:59 +0400

    Commit Committed

---
+++ src/components/ui/icons/icons.ts
@@ -0,0 +1,74 @@
+export const Icons = {
+  photo: {
+    paths: [
+      {
+        d: "m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z",
+      },
+    ],
+    class: "size-5 transition duration-500 ease-in-out group-hover:rotate-12",
+    height: 24,
+    width: 24,
+    fill: "currentColor",
+    viewBox: "0 0 24 24",
+    strokeWidth: "1.5",
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+    stroke: "currentColor",
+  },
+  reset: {
+    paths: [
+      {
+        d: "M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z",
+      },
+    ],
+    class: "size-6 transition duration-500 ease-in-out group-hover:rotate-[-360deg]",
+    height: 24,
+    width: 24,
+    fill: "currentColor",
+    viewBox: "0 -960 960 960",
+  },
+  download: {
+    paths: [
+      {
+        d: "M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3",
+      },
+    ],
+    class: "size-5 transition duration-500 ease-in-out group-hover:translate-y-0.5",
+    height: 24,
+    width: 24,
+    fill: "none",
+    viewBox: "0 0 24 24",
+    strokeWidth: "2",
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+    stroke: "currentColor",
+  },
+  random: {
+    paths: [
+      {
+        d: "M560-160v-80h104L537-367l57-57 126 126v-102h80v240H560Zm-344 0-56-56 504-504H560v-80h240v240h-80v-104L216-160Zm151-377L160-744l56-56 207 207-56 56Z",
+      },
+    ],
+    class: "size-5",
+    height: 24,
+    width: 24,
+    fill: "currentColor",
+    viewBox: "0 -960 960 960",
+  },
+  info: {
+    paths: [
+      {
+        d: "m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z",
+      },
+    ],
+    class: "size-5 text-sky-500 hs-tooltip-toggle cursor-pointer",
+    height: 24,
+    width: 24,
+    fill: "none",
+    viewBox: "0 0 24 24",
+    strokeWidth: "2",
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+    stroke: "currentColor",
+  },
+};
